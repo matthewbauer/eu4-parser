1fc3e23bfe837ac5f164b160660c6a0fc190dd9e
Matthew Justin Bauer <mjbauer95@gmail.com>
2016-08-24 17:53:32 -0500

    Update generate-positions.hs

--- generate-positions.hs
+++ generate-positions.hs
@@ -1,10 +1,10 @@
+-- generate a map of features to "positions" from the parsed assets
+
 import Prelude hiding (putStrLn)
 import Data.Aeson
 import Data.ByteString.Lazy.Char8 (putStrLn)
 import Map
 import Generator
-
--- instead of list this will generate dictionary for each position
 
 main :: IO ()
 main = putStrLn . encode =<< (genmapForwards <$> positions)
